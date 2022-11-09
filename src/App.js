f9f13e66ef5c8d79df9f24640ca7abb55a1dbec3
Ryan <rygho90@gmail.com>
2022-11-08 20:24:11 -0500

    Add NAV bar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { Container, Row, Col, Card, Navbar, Nav } from "react-bootstrap";
+
 function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <>
+      <Navbar bg="dark" variant="dark" className="p-2">
+        <Container>
+          <Navbar.Brand href="#">Guest's Library</Navbar.Brand>
+          <Nav>
+            <Nav.Link href="#" className=".text-light">
+              Log In
+            </Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
 }
 
 export default App;
