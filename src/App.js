14a8eb2e8b7b453a4524560d4dfc78f613353b39
Ryan <rygho90@gmail.com>
2022-11-08 20:54:05 -0500

    Add basic layout for book cards

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import { Container, Row, Col, Card, Navbar, Nav } from "react-bootstrap";
+import {
+  Container,
+  Row,
+  Col,
+  Card,
+  Navbar,
+  Nav,
+  Button,
+} from "react-bootstrap";
 
 function App() {
   return (
     <>
       <Navbar bg="dark" variant="dark" className="p-2">
         <Container>
-          <Navbar.Brand href="#">Guest's Library</Navbar.Brand>
+          <Navbar.Brand href="#">
+            <h1>Guest's Library</h1>
+          </Navbar.Brand>
           <Nav>
             <Nav.Link href="#" className=".text-light">
               Log In
@@ -13,6 +23,21 @@ function App() {
           </Nav>
         </Container>
       </Navbar>
+      <Container className="mt-3">
+        <Container className="d-flex align-items-center justify-content-center">
+          <Button>+ Add New Book</Button>
+        </Container>
+        <Container className="mt-3">
+          <Row xs={1} sm={2} md={3} lg={4} className="d-flex align-items-center justify-content-center">
+            <Card className="m-3" style={{ width: "18rem", height: "18rem" }}>Hello World</Card>
+            <Card className="m-3" style={{ width: "18rem", height: "18rem" }}>Hello World</Card>
+            <Card className="m-3" style={{ width: "18rem", height: "18rem" }}>Hello World</Card>
+            <Card className="m-3" style={{ width: "18rem", height: "18rem" }}>Hello World</Card>
+            <Card className="m-3" style={{ width: "18rem", height: "18rem" }}>Hello World</Card>
+            <Card className="m-3" style={{ width: "18rem", height: "18rem" }}>Hello World</Card>
+          </Row>
+        </Container>
+      </Container>
     </>
   );
 }
