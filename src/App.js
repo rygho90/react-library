83ba1639d119cfeed756dcb3c10c5742d744cfac
Ryan <rygho90@gmail.com>
2022-11-09 21:36:54 -0500

    Add basic modal for Add New Book button

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import {
-  Container,
-  Row,
-  Col,
-  Card,
-  Navbar,
-  Nav,
-  Button,
-} from "react-bootstrap";
+import { Container, Row, Navbar, Nav, Button, Modal } from "react-bootstrap";
+import React, { useState } from "react";
 import BookCard from "./components/BookCard";
 
 function App() {
+  const [showAddBookModal, setShowAddBookModal] = useState(false);
+
+  const handleCloseAddBookModal = () => setShowAddBookModal(false);
+  const handleShowAddBookModal = () => setShowAddBookModal(true);
+
   return (
     <>
       <Navbar bg="dark" variant="dark" className="p-2">
@@ -26,10 +24,16 @@ function App() {
       </Navbar>
       <Container className="mt-3">
         <Container className="d-flex align-items-center justify-content-center">
-          <Button>+ Add New Book</Button>
+          <Button onClick={handleShowAddBookModal}>+ Add New Book</Button>
         </Container>
         <Container className="mt-3">
-          <Row xs={1} sm={2} md={3} lg={4} className="d-flex align-items-center justify-content-center">
+          <Row
+            xs={1}
+            sm={2}
+            md={3}
+            lg={4}
+            className="d-flex align-items-center justify-content-center"
+          >
             <BookCard />
             <BookCard />
             <BookCard />
@@ -48,6 +52,23 @@ function App() {
           </Row>
         </Container>
       </Container>
+
+      <Modal
+        show={showAddBookModal}
+        onHide={handleCloseAddBookModal}
+        backdrop="static"
+        keyboard={false}
+      >
+        <Modal.Header closeButton>
+          <Modal.Title>Add new book</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          Hello World. It's me, ya boi.
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={handleCloseAddBookModal}>Close</Button>
+        </Modal.Footer>
+      </Modal>
     </>
   );
 }
